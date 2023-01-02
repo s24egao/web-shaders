c23069a84de9d59db4f4204e1cd2a3e011a516d9
s24egao <99890186+s24egao@users.noreply.github.com>
2023-01-02 21:27:35 +0800

    Update shader.js

--- shader.js
+++ shader.js
@@ -81,48 +81,37 @@ async function shader(id, vert, frag, data) {
 		1, 0,
 		1, 1, ], 2)
 
-	let mouse = gl.getUniformLocation(program, 'mouse')
-	gl.uniform2f(mouse, 0, 0)
-	canvas.addEventListener('mousemove', e => {
-		gl.uniform2f(mouse, e.offsetX / canvas.clientWidth, 1 - e.offsetY / canvas.clientHeight)
-	})
-
 	let ismousehover = false
 	let transition = 0
 	let mousehover = gl.getUniformLocation(program, 'mousehover')
-	canvas.addEventListener('mouseenter', () => {
-		ismousehover = true
-	})
-	canvas.addEventListener('mouseleave', () => {
-		ismousehover = false
-	})
+	canvas.addEventListener('mouseenter', () => ismousehover = true)
+	canvas.addEventListener('mouseleave', () =>  ismousehover = false)
 
 	let time = gl.getUniformLocation(program, 'time')
 	let t = 0
 	gl.uniform1f(time, 0)
 	setInterval(() => {
-		t += 0.02
+		t += 0.016
+		transition = lerp(transition, (ismousehover)? 1 : 0, 0.1)
 		gl.uniform1f(time, t)
-		if(ismousehover) transition = lerp(transition, 1, 0.1)
-		else transition = lerp(transition, 0, 0.1)
 		gl.uniform1f(mousehover, transition)
-	}, 20)
+	}, 16)
 
 	let url = data?.image
 	let images = []
 	if(Array.isArray(url)) for(let u of url) images.push(await(loadImage(gl, u)))
 	else if(url) images.push(await loadImage(gl, url))
 
-	for(let i = 0; i < images.length; i++) {
-		gl.activeTexture(gl.TEXTURE0 + i)
+	for(let i = 0; i < Math.min(images.length, 4); i++) {
+		gl.activeTexture([gl.TEXTURE0, gl.TEXTURE1, gl.TEXTURE2, gl.TEXTURE3][i])
 		gl.bindTexture(gl.TEXTURE_2D, images[i])
 		gl.uniform1i(gl.getUniformLocation(program, `texture${i}`), i)
 	}
 
 	function draw() {
+		requestAnimationFrame(draw)
 		gl.clear(gl.COLOR_BUFFER_BIT)
 		gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4)
-		requestAnimationFrame(draw)
 	}
 	if(!data?.dontAutoPlay) draw()
 
